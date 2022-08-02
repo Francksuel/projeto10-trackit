125e026b2e84a8557cf1b704261f8a63e4ceb7c1
Francksuel <francksuelbarbosa@gmail.com>
2022-08-02 19:08:39 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function App() {
+    return (
+        <h1>APP</h1>
+    )
+}
+const GlobalStyle = styled.div`
+font-family: 'Lexend Deca', sans-serif;
+`
